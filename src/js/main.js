2b1219d28a92c8b37a3e397c2d75d24e6e75f123
Roy Prigat <rp2719@columbia.edu>
2018-02-04 00:06:52 -0500

    fixed add button + modal

--- src/js/main.js
+++ src/js/main.js
@@ -1,45 +1,6 @@
-// Validate Form
-function validateForm(siteName, siteUrl) {
-  if (!siteName || !siteUrl) {
-    alert('Please fill in the form');
-    return false;
-  }
+// Listen for form submit
+document.getElementById('myForm').addEventListener('submit', saveBookmark);
 
-  const expression = /[-a-zA-Z0-9@:%_+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_+.~#?&//=]*)?/gi;
-  const regex = new RegExp(expression);
-
-  if (!siteUrl.match(regex)) {
-    alert('Please use a valid URL');
-    return false;
-  }
-
-  return true;
-}
-
-// Fetch bookmarks
-function fetchBookmarks() {
-  // Get bookmarks from localStorage
-  const bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
-  // Get output id
-  const bookmarksResults = document.getElementById('bookmarksResults');
-
-  // Build output
-  bookmarksResults.innerHTML = '';
-  for (let i = 0; i < bookmarks.length; i += 1) {
-    const name = bookmarks[i].name;
-    const url = bookmarks[i].url;
-
-    bookmarksResults.innerHTML += '<div class="well">' +
-      '<h3>' + name +
-      ' <a class="btn btn-default" target="_blank" href="' + url + '">Visit</a> ' +
-      ' <a onclick="deleteBookmark(\'' + url + '\')" class="btn btn-danger" href="#">Delete</a> ' +
-      '</h3>' +
-      '</div>';
-  }
-}
-
-
-// Save Bookmark
 function saveBookmark(e) {
   // Get form values
   const siteName = document.getElementById('siteName').value;
@@ -54,41 +15,25 @@ function saveBookmark(e) {
     url: siteUrl
   };
 
-
-  // Listen for form submit
-  document.getElementById('myForm').addEventListener('submit', saveBookmark);
-
-  /*
-    // Local Storage Test
-    localStorage.setItem('test', 'Hello World');
-    console.log(localStorage.getItem('test'));
-    localStorage.removeItem('test');
-    console.log(localStorage.getItem('test'));
-  */
-
   // Test if bookmarks is null
   if (localStorage.getItem('bookmarks') === null) {
-    // Init array
     const bookmarks = [];
-    // Add to array
     bookmarks.push(bookmark);
     // Set to localStorage
     localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
   } else {
     // Get bookmarks from localStorage
     const bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
-    // Add bookmark to array
     bookmarks.push(bookmark);
-    // Re-set back to localStorage
     localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
   }
 
   // Clear form
   document.getElementById('myForm').reset();
-
   // Re-fetch bookmarks
   fetchBookmarks();
-
+  // Hide modal
+  $('#addSubModal').modal('toggle');
   // Prevent form from submitting
   e.preventDefault();
 }
@@ -109,4 +54,44 @@ function deleteBookmark(url) {
 
   // Re-fetch bookmarks
   fetchBookmarks();
+}
+
+// Fetch bookmarks
+function fetchBookmarks() {
+  // Get bookmarks from localStorage
+  const bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+  // Get output id
+  const subscriptions = document.getElementById('subscriptions');
+
+  // Build output
+  subscriptions.innerHTML = '';
+  for (let i = 0; i < bookmarks.length; i += 1) {
+    const name = bookmarks[i].name;
+    const url = bookmarks[i].url;
+
+    subscriptions.innerHTML += '<div class="well">' +
+      '<h3>' + name +
+      ' <a class="btn btn-default" target="_blank" href="' + url + '">Visit</a> ' +
+      ' <a onclick="deleteBookmark(\'' + url + '\')" class="btn btn-danger delete" href="#">-</a> ' +
+      '</h3>' +
+      '</div>';
+  }
+}
+
+// Validate Form
+function validateForm(siteName, siteUrl) {
+  if (!siteName || !siteUrl) {
+    alert('Please fill in the form');
+    return false;
+  }
+
+  const expression = /[-a-zA-Z0-9@:%_+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_+.~#?&//=]*)?/gi;
+  const regex = new RegExp(expression);
+
+  if (!siteUrl.match(regex)) {
+    alert('Please use a valid URL');
+    return false;
+  }
+
+  return true;
 }
